666da6642628f329397200b90e3f81de2ec9d777
马成天江 <myxmctj@gmail.com>
2018-01-24 00:52:09 +0900

    Csv读取表完成

--- Air/Assets/Scripts/Data/Loader/CsvLoader.cs
+++ Air/Assets/Scripts/Data/Loader/CsvLoader.cs
@@ -1,35 +1,86 @@
-﻿//
-//  CsvLoad#FILEEXTENSION#
-//  #PROJECTNAME#
-//
-//  Created by #SMARTDEVELOPERS# on #CREATIONDATE#.
-//
-//
- 
- 
+﻿/** 
+ *Copyright(C) 2018 by #COMPANY# 
+ *All rights reserved. 
+ *FileName:     CsvLoader
+ *Author:       Passion 
+ *Version:      1.0 
+ *UnityVersion：2017.1.1f1 
+ *Date:         2018-01-23 
+ *Description:    
+ *History: 
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CsvLoader<T>  {
+public class CsvLoader
+{
 
-	private const string CsvPathRoot = "Csv";
+    private const string CsvPathRoot = "Csv/";
 
-	void LoadCsv(string path, DataBase<T> data) {
+    public static Dictionary<int, Dictionary<string, string>> LoadDataBaseCsv(string path)
+    {
+        var asset = Resources.Load(CsvPathRoot + path, typeof(TextAsset)) as TextAsset;
+
+        string value = asset.text;
+
+        //分割行
+        string[] strLine = value.Split('\n');
+        bool isLoadTypeEnd = false;
+        Dictionary<int, string> dataTypeTable = new Dictionary<int, string>();
+		Dictionary<int, Dictionary <string, string>> dataBase = new Dictionary<int, Dictionary<string, string>>();
+		int index = -1;
+
+        for (int i = 0; i < strLine.Length; i++)
+        {
+            string currentStrLine = strLine[i];
+            // 忽略行 忽略符号 #
+            if (currentStrLine.Substring(0, 1) == "#")
+            {
+                continue;
+            }
+            string[] currentDatas = currentStrLine.Split(',');
+            // 读取数据
+            Dictionary<string, string> data = new Dictionary<string, string>();
 
 
-		var asset = Resources.Load(CsvPathRoot + path, typeof(TextAsset)) as TextAsset;
+            for (int j = 0; j < currentDatas.Length; j++)
+            {
+                if (!isLoadTypeEnd)
+                {
+                    //  读取数据类型
+                    dataTypeTable.Add(j, currentDatas[j]);
 
-		string value = asset.text;
+                }
+                else
+                {
+                    // 登录各行数据
+					Debug.Log(dataTypeTable[j] + "," + currentDatas[j]);
+                    //data.Add(dataTypeTable[j], currentDatas[j]);
+					
+                }
+				
+            }
+			
 
-		//分割行
-		string[] strLine = value.Split('\n');
+            if (!isLoadTypeEnd)
+            {
+				for(int ii = 0; ii < dataTypeTable.Count; ii++) {
+                    // 登录
+                    dataBase.Add(ii, null);
+				}
+                isLoadTypeEnd = true;
+            } else {
+				dataBase[index] = data;
+            }
+        }
 
-		List<string> strText =  new List<string>();
 
-		for (int i = 0; i < strLine.Length; i++) {
-			if ()
-		}
-	}
-	
+
+        return dataBase;
+    }
+
+
 }
--- Air/Assets/Scripts/Managers/GameManager.cs
+++ Air/Assets/Scripts/Managers/GameManager.cs
@@ -1,12 +1,16 @@
-﻿//
-//  GameManager#FILEEXTENSION#
-//  #PROJECTNAME#
-//
-//  Created by #SMARTDEVELOPERS# on #CREATIONDATE#.
-//
-//
- 
- 
+﻿/** 
+ *Copyright(C) 2018 by #COMPANY# 
+ *All rights reserved. 
+ *FileName:     #SCRIPTFULLNAME# 
+ *Author:       #AUTHOR# 
+ *Version:      #VERSION# 
+ *UnityVersion：#UNITYVERSION# 
+ *Date:         #DATE# 
+ *Description:    
+ *History: 
+*/
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +18,7 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager> {
 
 	void Awake() {
+		Dictionary<int, Dictionary<string, string>> charaTable = CsvLoader.LoadDataBaseCsv("CharaDatas");
 		SceneManager.Instance.transform.SetParent(this.transform);
 	}
 
